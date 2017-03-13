a8f43c9e1e2d7562ad195bd69a7396f898b59bce
axereliat <axereliat@gmail.com>
2017-03-13 09:47:14 +0200

    Initial commit
    Signed-off-by: axereliat <axereliat@gmail.com>

---
+++ Blog/Controllers/Admin/UserController.cs
@@ -0,0 +1,206 @@
+﻿using Blog.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers.Admin
+{
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index() {
+            return RedirectToAction("List");
+        }
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var users = database.Users
+                    .ToList();
+
+                var admins = GetAdminUserNames(users, database);
+                ViewBag.Admins = admins;
+
+                return View(users);
+            }
+        }
+
+        private HashSet<string> GetAdminUserNames(List<ApplicationUser> users, BlogDbContext context) {
+            var userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+
+            var admins = new HashSet<string>();
+
+            foreach (var user in users)
+            {
+                if (userManager.IsInRole(user.Id, "Admin")) {
+                    admins.Add(user.UserName);
+                }
+            }
+
+            return admins;
+        }
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var user = database.Users
+                    .Where(u => u.Id == id)
+                    .First();
+
+                if (user == null)
+                {
+                    return HttpNotFound();
+                }
+
+                var viewModel = new EditUserViewModel();
+                viewModel.User = user;
+                viewModel.Roles = GetUserRoles(user, database);
+
+                return View(viewModel);
+            }
+        }
+
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var user = database.Users
+                    .Where(u => u.Id.Equals(id))
+                    .First();
+
+                if (user == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(user);
+            }
+        }
+
+        // Post: User/Edit
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var user = database.Users
+                    .Where(u => u.Id.Equals(id))
+                    .First();
+
+                var userArticles = database.Articles
+                    .Where(a => a.Author.Id == user.Id);
+
+                foreach (var article in userArticles)
+                {
+                    database.Articles.Remove(article);
+                }
+
+                database.Users.Remove(user);
+                database.SaveChanges();
+
+                return RedirectToAction("List");
+            }
+        }
+
+        // Post: User/Edit
+        [HttpPost]
+        public ActionResult Edit(string id, EditUserViewModel viewModel)
+        {
+            if (ModelState.IsValid) {
+                using (var database = new BlogDbContext()) {
+                    var user = database.Users.FirstOrDefault(u => u.Id == id);
+                    if (user == null) {
+                        return HttpNotFound();
+                    }
+
+                    if (!string.IsNullOrEmpty(viewModel.Password)) {
+                        var hasher = new PasswordHasher();
+                        var passwordHash = hasher.HashPassword(viewModel.Password);
+                        user.PasswordHash = passwordHash;
+                    }
+
+                    user.Email = viewModel.User.Email;
+                    user.FullName = viewModel.User.FullName;
+                    user.UserName = viewModel.User.Email;
+                    this.SetUserRoles(user, database, viewModel);
+
+                    database.Entry(user).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    return RedirectToAction("List");
+                }
+            }
+            return View(viewModel);
+        }
+
+        private void SetUserRoles(ApplicationUser user, BlogDbContext db, EditUserViewModel model)
+        {
+            var userManager = Request
+                .GetOwinContext()
+                .GetUserManager<ApplicationUserManager>();
+
+            foreach (var role in model.Roles)
+            {
+                if (role.isSelected)
+                {
+                    userManager.AddToRole(user.Id, role.Name);
+                }
+                else if (!role.isSelected) {
+                    userManager.RemoveFromRole(user.Id, role.Name);
+                }
+            }
+        }
+
+        private IList<Role> GetUserRoles(ApplicationUser user, BlogDbContext db)
+        {
+            var userManager = Request
+                .GetOwinContext()
+                .GetUserManager<ApplicationUserManager>();
+
+            var roles = db.Roles
+                .Select(r => r.Name)
+                .OrderBy(r => r)
+                .ToList();
+
+            var userRoles = new List<Role>();
+            foreach (var roleName in roles)
+            {
+                var role = new Role { Name = roleName };
+
+                if (userManager.IsInRole(user.Id, roleName))
+                {
+                    role.isSelected = true;
+                }
+
+                userRoles.Add(role);
+            }
+            return userRoles;
+        }
+    }
+}
---
+++ Blog/Controllers/ArticleController.cs
@@ -0,0 +1,256 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TimeAgo;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        // GET: /Article/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var articles = database.Articles
+                    .Include(a => a.Author)
+                    .Include(a => a.Tags)
+                    .Include(a => a.Category)
+                    .ToList();
+
+                return View(articles);
+            }
+        }
+
+        public ActionResult Details(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext()) {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .Include(a => a.Tags)
+                    .First();
+
+                if (article == null) {
+                    return HttpNotFound();
+                }
+
+                return View(article);
+            }
+        }
+
+        // GET: /Article/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                if (!IsUserAuthorizedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                var model = new ArticleViewModel();
+                model.Id = article.Id;
+                model.Content = article.Content;
+                model.Title = article.Title;
+                model.CategoryId = article.CategoryId;
+                model.Categories = database.Categories
+                    .OrderBy(c => c.Name)
+                    .ToList();
+
+                model.Tags = string.Join(", ", article.Tags.Select(t => t.Name));
+
+                return View(model);
+            }
+        }
+
+        // POST: /Article/Edit
+        [HttpPost]
+        public ActionResult Edit(ArticleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    var article = database.Articles
+                        .FirstOrDefault(a => a.Id == model.Id);
+
+
+                    article.Id = model.Id;
+                    article.Content = model.Content;
+                    article.Title = model.Title;
+                    article.CategoryId = model.CategoryId;
+                    this.SetArticleTags(article, model, database);
+
+                    database.Entry(article).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+        }
+
+        private void SetArticleTags(Article article, ArticleViewModel model, BlogDbContext db)
+        {
+            var tagsStrings = model.Tags
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(t => t.ToLower())
+                .Distinct();
+
+            article.Tags.Clear();
+
+            foreach (var tagString in tagsStrings)
+            {
+                Tag tag = db.Tags.FirstOrDefault(t => t.Name.Equals(tagString));
+
+                if (tag == null) {
+                    tag = new Tag() { Name = tagString };
+                    db.Tags.Add(tag);
+                }
+
+                article.Tags.Add(tag);
+            }
+        }
+
+        // Get: Article/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var model = new ArticleViewModel();
+                model.Categories = database.Categories
+                    .OrderBy(c => c.Name)
+                    .ToList();
+
+                return View(model);
+            }
+        }
+
+        // Post: Article/Create
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(ArticleViewModel model) {
+            if (ModelState.IsValid) {
+                using (var database = new BlogDbContext())
+                {
+                    var authorId = database.Users
+                        .Where(u => u.UserName == this.User.Identity.Name)
+                        .First()
+                        .Id;
+
+                    var article = new Article(authorId, model.Title, model.Content, model.CategoryId);
+                    
+                    article.DateAdded = DateTime.Now;
+
+                    this.SetArticleTags(article, model, database);
+
+                    database.Articles.Add(article);
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+        }
+
+        // Get: Article/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .Include(a => a.Category)
+                    .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                if (!IsUserAuthorizedToEdit(article)) {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                ViewBag.TagsString = string.Join(", ", article.Tags.Select(t => t.Name));
+
+                return View(article);
+            }
+        }
+
+        // Post: Article/Delete
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                database.Articles.Remove(article);
+                database.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        private bool IsUserAuthorizedToEdit(Article article)
+        {
+            bool isAdmin = this.User.IsInRole("Admin");
+            bool isAuthor = article.IsAuthor(this.User.Identity.Name);
+
+            return isAdmin || isAuthor;
+        }
+    }
+}
